771170335ab0c4309d2c04a319c292c283124122
Steven Thompson <steven@fantasmagorical.co.uk>
2013-06-25 23:49:02 +0100

    Initial commit of URL shortening component for CakePHP 2.0 using bitly.com

---
+++ UrlComponent.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * --- URL Component ---
+ * Github: https://github.com/steven/Cakephp-2.0.Component-Crypt
+ * For generating a short URL by using bitly.com
+ * You will need a free account from https://bitly.com/ to set up your API credentials
+ *
+ * @author Steven Thompson <steven@fantasmagorical.co.uk>
+ */
+ 
+class UrlComponent extends Component
+{
+	var $login = 'LOGINUSERNAME';
+	var $apiKey = 'APIKEY';
+	
+	function shorten($url) {
+		$api_call = file_get_contents("http://api.bit.ly/shorten?version=2.0.1&longUrl=".$url."&login=".$bitly_login."&apiKey=".$bitly_apikey);
+		$bitlyinfo=json_decode(utf8_encode($api_call),true);
+	
+		if ($bitlyinfo['errorCode']==0) {
+			return $bitlyinfo['results'][urldecode($url)]['shortUrl'];
+		} else {
+			return false;
+		}
+	}
+} ?>
